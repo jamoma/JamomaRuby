c8339bb9b767611bb307f7503dc77c01bf0239cc
+1m place <tim@74objects.com>
2014-03-04 10:23:08 +0100

    build.rb: do not try to build (and thus crash ruby) on Windows.

--- build.rb
+++ build.rb
@@ -1,16 +1,19 @@
 #!/usr/bin/env ruby -wKU
 # encoding: utf-8
 
-puts "Building Ruby Language Extensions..."
- 
-if (RUBY_PLATFORM == "universal-darwin9.0")
-  output = `cd library; ARCHFLAGS="-arch i386" ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
+if win?
+	puts "Ruby support is not currently built for Windows.  Please volunteer to help us!"
 else
-  `cd library; ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
-end
+	puts "Building Ruby Language Extensions..."
+	 
+	if (RUBY_PLATFORM == "universal-darwin9.0")
+	  output = `cd library; ARCHFLAGS="-arch i386" ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
+	else
+	  `cd library; ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
+	end
 
-load "install.rb"
+	load "install.rb"
 
-puts
-puts "=================DONE===================="
-  
+	puts
+	puts "=================DONE===================="
+ end
