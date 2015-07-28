f3aa1e19b84d44f1d7576ee781daeaa125712ad8
Timothy Place <tim@cycling74.com>
2015-07-28 15:59:54 -0600

    build.rb: stripping out commands to rebuild the Core -- so build that manually first for now to keep it simple.

--- build.rb
+++ build.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/env ruby -wKU
 # encoding: utf-8
 
-#if win?
-#	puts "Ruby support is not currently built for Windows.  Please volunteer to help us!"
-#else
-	puts "Building Ruby Language Extensions..."
-	 
-	if (RUBY_PLATFORM == "universal-darwin9.0")
-	  output = `cd library; ARCHFLAGS="-arch i386" ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
-	else
-	  `cd library; ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
-	end
+#puts "First we must build JamomaCore.  This could take a while."
+#puts "Then we will build the Jamoma Ruby Extension."
+#puts
+#
+ruby_root_dir = Dir.pwd
+#dir.chdir "#{ruby_root_dir}/JamomaCore"
+#load "build.rb"
 
-	load "install.rb"
+puts "Building Ruby Language Extensions..."
+puts
+ 
+Dir.chdir ruby_root_dir
+if (RUBY_PLATFORM == "universal-darwin9.0")
+  output = `cd library; ARCHFLAGS="-arch i386" ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
+else
+  `cd library; ruby extconf.rb; make clean; make; ruby postbuild.rb; cd ..`
+end  
+load "install.rb"
 
-	puts
-	puts "=================DONE===================="
-# end
+puts
+puts "=================DONE===================="
+puts
